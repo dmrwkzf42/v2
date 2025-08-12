e8af8bdfcae7a05edccb5ce3e4f7a656c874d398
dmrwkzf42 <dmrwkzf42@ghostermail.com>
2025-08-12 11:50:03 -0400

    Update feature.go
    fix bug

--- features/feature.go
+++ features/feature.go
@@ -7,8 +7,8 @@ import "v2ray.com/core/common"
 // Feature is the interface for V2Ray features. All features must implement this interface.
 // All existing features have an implementation in app directory. These features can be replaced by third-party ones.
 type Feature interface {
-	common.HasType
-	common.Runnable
+	common.HasType2
+	common.Runnable2
 }
 
 // PrintDeprecatedFeatureWarning prints a warning for deprecated feature.
